9c4950135759170ae9c4b7dcacca86e982e84c55
Ersin Köseoğlu <ersinkoseoglu38@gmail.com>
2023-09-18 20:06:05 +0300

    Camera Controller

--- CityBuildingGame/Assets/Scripts/CameraController.cs
+++ CityBuildingGame/Assets/Scripts/CameraController.cs
@@ -4,15 +4,82 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed;
+
+    #region CameraRotationVariables
+    public float minXRot;
+    public float maxXRot;
+    private float curXRot;
+    public float rotateSpeed;
+    #endregion
+
+    #region CamerazoomVariables
+    public float minZoom;
+    public float maxZoom;
+    private float curZoom;
+    public float zoomSpeed;
+    #endregion
+
+    private Camera cam;
+
+    private void Start()
     {
-        
+        cam = Camera.main;
+        curZoom = cam.transform.localPosition.y;
+        curXRot = -50;
+    }
+    private void Update()
+    {
+        //Zoom
+        CameraZoom();
+
+        //Rotation
+        CameraRotation();
+
+        //Movement
+        CameraMovement();
+
+    }
+    void CameraZoom()
+    {
+        curZoom += Input.GetAxis("Mouse ScrollWheel") * -zoomSpeed;
+        curZoom = Mathf.Clamp(curZoom, minZoom, maxZoom);
+
+        cam.transform.localPosition = Vector3.up * curZoom;
+    }
+    void CameraRotation()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float x = Input.GetAxis("Mouse X");
+            float y = Input.GetAxis("Mouse Y");
+
+            curXRot += -y * rotateSpeed;
+            curXRot = Mathf.Clamp(curXRot, minXRot, maxXRot);
+
+            transform.eulerAngles = new Vector3(curXRot, transform.eulerAngles.y + (x * rotateSpeed), 0);
+        }
+    }
+    void CameraMovement()
+    {
+        Vector3 forward = cam.transform.forward;
+        forward.y = 0;
+        forward.Normalize();
+
+        Vector3 right = cam.transform.right;
+
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = forward * moveZ + right * moveX;
+
+        dir.Normalize();
+
+        dir *= moveSpeed * Time.deltaTime;
+
+        transform.position += dir;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
